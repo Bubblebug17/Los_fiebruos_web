eacda137c8d5ee5c59b7b404bcf39e7d8fea8119
Bubblebug17 <elias.monzon.sm@gmail.com>
2025-10-02 20:02:01 -0500

    - Se agregó librería VANTA.js - Avance de página "Los Rajones" - Se agregó reseña en "El Nexo" de "Chronos Echoes"

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,19 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+VANTA.NET({
+    el: "#body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x00a2ff, // Azure vibrante de la paleta
+    backgroundColor: 0x12121200, // Fondo oscuro de la paleta
+    points: 20.00,
+    maxDistance: 23.00,
+    spacing: 17.00
+  })
